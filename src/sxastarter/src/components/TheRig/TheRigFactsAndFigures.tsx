2f77a61daf25702081d224228105e3aa4123fd1d
sc-johanbecue <johan.becue@sitecore.com>
2024-03-15 14:59:33 +0100

    modified:   src/sxastarter/src/components/TheRig/TheRigFactsAndFigures.tsx

--- src/sxastarter/src/components/TheRig/TheRigFactsAndFigures.tsx
+++ src/sxastarter/src/components/TheRig/TheRigFactsAndFigures.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Text, TextField } from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
   Title: TextField;
@@ -47,7 +47,7 @@ export const Default = (props: TheRigFactAndFiguresroProps): JSX.Element => {
                 className="MuiTypography-root MuiTypography-h2 css-1y5qbxs"
                 style={{ textAlign: 'center' }}
               >
-                FACTS &amp; FIGURES
+                <Text field={props.fields.Title} />
               </h2>
             </div>
           </div>
@@ -58,46 +58,67 @@ export const Default = (props: TheRigFactAndFiguresroProps): JSX.Element => {
               <style data-emotion="css 1wb1tnx">{`.css-1wb1tnx{border-right:2px solid #777777;}.css-1wb1tnx .breakLine{display:none;}@media screen and (max-width: 899px){.css-1wb1tnx{border-right:none;}.css-1wb1tnx .breakLine{display:block;width:100px;margin:10px auto;}.css-1wb1tnx .last{display:none;}}`}</style>
               <div className="css-1wb1tnx">
                 <style data-emotion="css 1q2jcol">{`.css-1q2jcol{font-family:'Changa',sans-serif;font-weight:400;color:#FF6A39;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:center;-ms-flex-pack:center;-webkit-justify-content:center;justify-content:center;font-weight:800;font-size:84px;line-height:60px;padding-top:40px;position:relative;text-align:center;}@media screen and (max-width: 1439px){.css-1q2jcol{font-size:48px;line-height:68px;padding-top:0;}}.css-1q2jcol span{font-size:2.2rem;font-family:'Changa',sans-serif;font-weight:400;color:inherit;font-weight:800;text-transform:uppercase;margin-left:8px;}@media screen and (max-width: 899px){.css-1q2jcol span{font-size:1.8rem;margin-left:6px;}}`}</style>
-                <div className="css-1q2jcol">+300K</div>
+                <div className="css-1q2jcol">
+                  <Text field={props.fields.Fact1Figure} />
+                </div>
                 <style data-emotion="css 1b171hf">{`.css-1b171hf{font-family:'Changa',sans-serif;font-weight:400;color:#FF6A39;font-weight:500;text-align:center;padding-top:10px;}.css-1b171hf span{font-size:18px;font-family:'Changa',sans-serif;font-weight:400;color:#ffffff;font-size:24px;font-style:normal;font-weight:500;line-height:160%;}@media screen and (max-width: 1439px){.css-1b171hf span{font-size:14px;padding-top:0px;}}`}</style>
                 <div className="css-1b171hf">
-                  <span>Gross floor area (m2)</span>
+                  <span>
+                    <Text field={props.fields.Fact1Text} />
+                  </span>
                 </div>
                 <hr className="breakLine " />
               </div>
             </div>
             <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-2.4 MuiGrid-grid-lg-2.4 en-grid-item css-1q09act">
               <div className="css-1wb1tnx">
-                <div className="css-1q2jcol">3</div>
+                <div className="css-1q2jcol">
+                  <Text field={props.fields.Fact2Figure} />
+                </div>
                 <div className="css-1b171hf">
-                  <span>Hotels</span>
+                  <span>
+                    <Text field={props.fields.Fact2Text} />
+                  </span>
                 </div>
                 <hr className="breakLine " />
               </div>
             </div>
             <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-2.4 MuiGrid-grid-lg-2.4 en-grid-item css-1q09act">
               <div className="css-1wb1tnx">
-                <div className="css-1q2jcol">800</div>
+                <div className="css-1q2jcol">
+                  <Text field={props.fields.Fact3Figure} />
+                </div>
                 <div className="css-1b171hf">
-                  <span>Hotel Rooms</span>
+                  <span>
+                    <Text field={props.fields.Fact3Text} />
+                  </span>
                 </div>
                 <hr className="breakLine " />
               </div>
             </div>
             <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-2.4 MuiGrid-grid-lg-2.4 en-grid-item css-1q09act">
               <div className="css-1wb1tnx">
-                <div className="css-1q2jcol">11</div>
+                <div className="css-1q2jcol">
+                  <Text field={props.fields.Fact4Figure} />
+                </div>
                 <div className="css-1b171hf">
-                  <span>Restaurants</span>
+                  <span>
+                    {' '}
+                    <Text field={props.fields.Fact4Text} />
+                  </span>
                 </div>
                 <hr className="breakLine " />
               </div>
             </div>
             <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-2.4 MuiGrid-grid-lg-2.4 en-grid-item css-1q09act">
               <div className="css-1wb1tnx">
-                <div className="css-1q2jcol">+70</div>
+                <div className="css-1q2jcol">
+                  <Text field={props.fields.Fact5Figure} />
+                </div>
                 <div className="css-1b171hf">
-                  <span>Attractions &amp; Activities</span>
+                  <span>
+                    <Text field={props.fields.Fact5Text} />
+                  </span>
                 </div>
                 <hr className="breakLine last" />
               </div>
