09fe1988db9932f85ffc145184fc77d0734c0910
sc-johanbecue <johan.becue@sitecore.com>
2024-03-17 00:53:32 +0100

    modified:   src/sxastarter/src/LayoutTheRig.tsx 	modified:   src/sxastarter/src/components/TheRig/TheRigESG.tsx 	modified:   src/sxastarter/src/components/TheRig/TheRigFactsAndFigures.tsx 	modified:   src/sxastarter/src/components/TheRig/TheRigFooter.tsx 	modified:   src/sxastarter/src/components/TheRig/TheRigHeader.tsx 	modified:   src/sxastarter/src/components/TheRig/TheRigIntro.tsx

--- src/sxastarter/src/components/TheRig/TheRigHeader.tsx
+++ src/sxastarter/src/components/TheRig/TheRigHeader.tsx
@@ -30,7 +30,7 @@ export const Default = (props: TheRigHeaderProps): JSX.Element => {
   if (props.fields) {
     return (
       <header className=" css-cea17v">
-        <style data-emotion="css 58w1wt">
+        <style jsx data-emotion="css 58w1wt">
           {`
             .css-58w1wt {
               display: -webkit-box;
@@ -45,7 +45,7 @@ export const Default = (props: TheRigHeaderProps): JSX.Element => {
         <div className="css-58w1wt">
           <div className="css-0">
             <Link href="/">
-              <style data-emotion="css xzc2ri">
+              <style jsx data-emotion="css xzc2ri">
                 {`
                   .css-xzc2ri {
                     -webkit-transition: 0.3s;
@@ -66,47 +66,47 @@ export const Default = (props: TheRigHeaderProps): JSX.Element => {
               />
             </Link>
           </div>
-          <style data-emotion="css f8bp36">
+          <style jsx data-emotion="css f8bp36">
             {`
-               .css-f8bp36{
-                  -webkit-box-flex:1;
-                  -webkit-flex-grow:1;
-                  -ms-flex-positive:1;
-                  flex-grow:1;
-                  text-align:center;
-                  -webkit-padding-end:92px;
-                  padding-inline-end:92px;
-                  display:-webkit-box;
-                  display:-webkit-flex;
-                  display:-ms-flexbox;
-                  display:flex;
-                  -webkit-align-items:center;
-                  -webkit-box-align:center;
-                  -ms-flex-align:center;
-                  align-items:center;
-                  -webkit-box-pack:center;
-                  -ms-flex-pack:center;
-                  -webkit-justify-content:center;
-                  justify-content:center;
-               }
-               @media screen and (max-width: 899px){
-                  .css-f8bp36{
-                     -webkit-padding-end:28px;
-                     padding-inline-end:28px;
-                     -webkit-align-items:flex-start;
-                     -webkit-box-align:flex-start;
-                     -ms-flex-align:flex-start;
-                     align-items:flex-start;
-                     -webkit-box-pack:end;
-                     -ms-flex-pack:end;
-                     -webkit-justify-content:end;
-                     justify-content:end;
-                  }
-               }
+              .css-f8bp36 {
+                -webkit-box-flex: 1;
+                -webkit-flex-grow: 1;
+                -ms-flex-positive: 1;
+                flex-grow: 1;
+                text-align: center;
+                -webkit-padding-end: 92px;
+                padding-inline-end: 92px;
+                display: -webkit-box;
+                display: -webkit-flex;
+                display: -ms-flexbox;
+                display: flex;
+                -webkit-align-items: center;
+                -webkit-box-align: center;
+                -ms-flex-align: center;
+                align-items: center;
+                -webkit-box-pack: center;
+                -ms-flex-pack: center;
+                -webkit-justify-content: center;
+                justify-content: center;
+              }
+              @media screen and (max-width: 899px) {
+                .css-f8bp36 {
+                  -webkit-padding-end: 28px;
+                  padding-inline-end: 28px;
+                  -webkit-align-items: flex-start;
+                  -webkit-box-align: flex-start;
+                  -ms-flex-align: flex-start;
+                  align-items: flex-start;
+                  -webkit-box-pack: end;
+                  -ms-flex-pack: end;
+                  -webkit-justify-content: end;
+                  justify-content: end;
+                }
+              }
             `}
           </style>
           <div className="css-f8bp36">
-            <style data-emotion="css lqzyi5">
+            <style jsx data-emotion="css lqzyi5">
               {`
                 .css-lqzyi5 {
                   max-width: 530px;
@@ -138,7 +138,7 @@ export const Default = (props: TheRigHeaderProps): JSX.Element => {
             </style>
             <nav className="css-lqzyi5">
               <div dir="ltr">
-                <style data-emotion="css an3xli">
+                <style jsx data-emotion="css an3xli">
                   {`
                     .css-an3xli {
                       font-family: 'Changa', sans-serif !important;
@@ -180,7 +180,7 @@ export const Default = (props: TheRigHeaderProps): JSX.Element => {
                 </style>
                 <Link className=" css-an3xli" href="/about/">
                   ABOUT THE RIG.
-                  <style data-emotion="css th6fei">
+                  <style jsx data-emotion="css th6fei">
                     {`
                       .css-th6fei {
                         position: absolute;
@@ -208,7 +208,7 @@ export const Default = (props: TheRigHeaderProps): JSX.Element => {
                   <div className="underline css-th6fei"></div>
                 </Link>
               </div>
-              <style data-emotion="css r2sx70">
+              <style jsx data-emotion="css r2sx70">
                 {`
                   .css-r2sx70 {
                     cursor: pointer;
@@ -223,7 +223,7 @@ export const Default = (props: TheRigHeaderProps): JSX.Element => {
               <button className="css-r2sx70">
                 <img src="https://therig.sa/svg/moon-icon.svg" alt="" />
               </button>
-              <style data-emotion="css 1tssrit">
+              <style jsx data-emotion="css 1tssrit">
                 {`
                   .css-1tssrit {
                     font-family: 'Changa', sans-serif !important;
@@ -256,7 +256,7 @@ export const Default = (props: TheRigHeaderProps): JSX.Element => {
                 <div className="underline css-th6fei"></div>
               </Link>
             </nav>
-            <style data-emotion="css 9qr6hs">
+            <style jsx data-emotion="css 9qr6hs">
               {`
                 .css-9qr6hs {
                   position: absolute;
--- src/sxastarter/src/components/TheRig/TheRigIntro.tsx
+++ src/sxastarter/src/components/TheRig/TheRigIntro.tsx
@@ -29,16 +29,127 @@ export const Default = (props: TheRigIntroProps): JSX.Element => {
   if (props.fields) {
     return (
       <div className="intro-wrapper css-19tk58b">
-        <style data-emotion="css 179ykmo">{`.css-179ykmo{position:absolute;top:0px;width:100%;}`}</style>
+        <style jsx data-emotion="css 179ykmo">{`
+          .css-179ykmo {
+            position: absolute;
+            top: 0px;
+            width: 100%;
+          }
+        `}</style>
         <img src="https://therig.sa/bg/linear.webp" className="css-179ykmo" alt="" />
-        <style data-emotion="css 1f1qt3z">{`.css-1f1qt3z{max-width:1440px!important;}@media screen and (max-width: 899px){.css-1f1qt3z{padding:0 24px!important;}}`}</style>
-        <style data-emotion="css sjot2u">{`.css-sjot2u{width:100%;margin-left:auto;box-sizing:border-box;margin-right:auto;display:block;padding-left:16px;padding-right:16px;max-width:1440px!important;}@media (min-width:600px){.css-sjot2u{padding-left:24px;padding-right:24px;}}@media (min-width:1200px){.css-sjot2u{max-width:1200px;}}@media screen and (max-width: 899px){.css-sjot2u{padding:0 24px!important;}}`}</style>
+        <style jsx data-emotion="css 1f1qt3z">{`
+          .css-1f1qt3z {
+            max-width: 1440px !important;
+          }
+          @media screen and (max-width: 899px) {
+            .css-1f1qt3z {
+              padding: 0 24px !important;
+            }
+          }
+        `}</style>
+        <style jsx data-emotion="css sjot2u">{`
+          .css-sjot2u {
+            width: 100%;
+            margin-left: auto;
+            box-sizing: border-box;
+            margin-right: auto;
+            display: block;
+            padding-left: 16px;
+            padding-right: 16px;
+            max-width: 1440px !important;
+          }
+          @media (min-width: 600px) {
+            .css-sjot2u {
+              padding-left: 24px;
+              padding-right: 24px;
+            }
+          }
+          @media (min-width: 1200px) {
+            .css-sjot2u {
+              max-width: 1200px;
+            }
+          }
+          @media screen and (max-width: 899px) {
+            .css-sjot2u {
+              padding: 0 24px !important;
+            }
+          }
+        `}</style>
         <div className="MuiContainer-root MuiContainer-maxWidthLg css-sjot2u">
-          <style data-emotion="css 124fws5">{`.css-124fws5{min-width:0;box-sizing:border-box;padding:calc(var(--Grid-rowSpacing) / 2) calc(var(--Grid-columnSpacing) / 2);}`}</style>
+          <style jsx data-emotion="css 124fws5">{`
+            .css-124fws5 {
+              min-width: 0;
+              box-sizing: border-box;
+              padding: calc(var(--Grid-rowSpacing) / 2) calc(var(--Grid-columnSpacing) / 2);
+            }
+          `}</style>
           <div className="MuiGrid-root MuiGrid-direction-xs-row css-124fws5">
-            <style data-emotion="css 1ug0hhs">{`.css-1ug0hhs{width:866px;z-index:20;text-align:center;position:absolute;left:50%;top:60%;-webkit-transform:translate(-60%, -60%);-webkit-transform:translate(-50%, -50%);-moz-transform:translate(-50%, -50%);-ms-transform:translate(-50%, -50%);transform:translate(-50%, -50%);}@media screen and (min-width: 1200px){.css-1ug0hhs{margin:auto;}}@media screen and (min-width: 1601px){.css-1ug0hhs{top:55%;-webkit-transform:translate(-50%, -40%);-moz-transform:translate(-50%, -40%);-ms-transform:translate(-50%, -40%);transform:translate(-50%, -40%);}}@media screen and (max-width: 899px){.css-1ug0hhs{top:50%;width:auto;}}`}</style>
+            <style jsx data-emotion="css 1ug0hhs">{`
+              .css-1ug0hhs {
+                width: 866px;
+                z-index: 20;
+                text-align: center;
+                position: absolute;
+                left: 50%;
+                top: 60%;
+                -webkit-transform: translate(-60%, -60%);
+                -webkit-transform: translate(-50%, -50%);
+                -moz-transform: translate(-50%, -50%);
+                -ms-transform: translate(-50%, -50%);
+                transform: translate(-50%, -50%);
+              }
+              @media screen and (min-width: 1200px) {
+                .css-1ug0hhs {
+                  margin: auto;
+                }
+              }
+              @media screen and (min-width: 1601px) {
+                .css-1ug0hhs {
+                  top: 55%;
+                  -webkit-transform: translate(-50%, -40%);
+                  -moz-transform: translate(-50%, -40%);
+                  -ms-transform: translate(-50%, -40%);
+                  transform: translate(-50%, -40%);
+                }
+              }
+              @media screen and (max-width: 899px) {
+                .css-1ug0hhs {
+                  top: 50%;
+                  width: auto;
+                }
+              }
+            `}</style>
             <div className="css-1ug0hhs">
-              <style data-emotion="css 1vtk6j6">{`.css-1vtk6j6{-webkit-filter:none;filter:none;}@media screen and (max-width: 899px){.css-1vtk6j6{width:327px;height:100px;}}@media screen and (min-width: 900px){.css-1vtk6j6{width:570px;height:120px;}}@media screen and (min-width: 1200px){.css-1vtk6j6{width:570px;height:120px;}}@media screen and (min-width: 1440px){.css-1vtk6j6{width:570px;height:120px;}}`}</style>
+              <style jsx data-emotion="css 1vtk6j6">{`
+                .css-1vtk6j6 {
+                  -webkit-filter: none;
+                  filter: none;
+                }
+                @media screen and (max-width: 899px) {
+                  .css-1vtk6j6 {
+                    width: 327px;
+                    height: 100px;
+                  }
+                }
+                @media screen and (min-width: 900px) {
+                  .css-1vtk6j6 {
+                    width: 570px;
+                    height: 120px;
+                  }
+                }
+                @media screen and (min-width: 1200px) {
+                  .css-1vtk6j6 {
+                    width: 570px;
+                    height: 120px;
+                  }
+                }
+                @media screen and (min-width: 1440px) {
+                  .css-1vtk6j6 {
+                    width: 570px;
+                    height: 120px;
+                  }
+                }
+              `}</style>
               <img
                 alt=""
                 loading="lazy"
@@ -51,8 +162,52 @@ export const Default = (props: TheRigIntroProps): JSX.Element => {
                 src="https://therig.sa/headerTitle/en-mbl-hero.svg"
               />
               <div className="MuiGrid-root MuiGrid-direction-xs-row css-124fws5">
-                <style data-emotion="css srloyh">{`.css-srloyh{font-family:'Changa',sans-serif;font-weight:400;text-align:center;margin:16px;font-size:24px;font-style:normal;font-weight:400;line-height:160%;color:#E1E5EE;}@media screen and (max-width: 599px){.css-srloyh{font-size:18px;line-height:160%;margin:0px 16px 16px;}}`}</style>
-                <style data-emotion="css pheo6p">{`.css-pheo6p{margin:0;font-family:"Roboto","Helvetica","Arial",sans-serif;font-weight:400;font-size:1rem;line-height:1.5;letter-spacing:0.00938em;font-family:'Changa',sans-serif;font-weight:400;text-align:center;margin:16px;font-size:24px;font-style:normal;font-weight:400;line-height:160%;color:#E1E5EE;}@media screen and (max-width: 599px){.css-pheo6p{font-size:18px;line-height:160%;margin:0px 16px 16px;}}`}</style>
+                <style jsx data-emotion="css srloyh">{`
+                  .css-srloyh {
+                    font-family: 'Changa', sans-serif;
+                    font-weight: 400;
+                    text-align: center;
+                    margin: 16px;
+                    font-size: 24px;
+                    font-style: normal;
+                    font-weight: 400;
+                    line-height: 160%;
+                    color: #e1e5ee;
+                  }
+                  @media screen and (max-width: 599px) {
+                    .css-srloyh {
+                      font-size: 18px;
+                      line-height: 160%;
+                      margin: 0px 16px 16px;
+                    }
+                  }
+                `}</style>
+                <style jsx data-emotion="css pheo6p">{`
+                  .css-pheo6p {
+                    margin: 0;
+                    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+                    font-weight: 400;
+                    font-size: 1rem;
+                    line-height: 1.5;
+                    letter-spacing: 0.00938em;
+                    font-family: 'Changa', sans-serif;
+                    font-weight: 400;
+                    text-align: center;
+                    margin: 16px;
+                    font-size: 24px;
+                    font-style: normal;
+                    font-weight: 400;
+                    line-height: 160%;
+                    color: #e1e5ee;
+                  }
+                  @media screen and (max-width: 599px) {
+                    .css-pheo6p {
+                      font-size: 18px;
+                      line-height: 160%;
+                      margin: 0px 16px 16px;
+                    }
+                  }
+                `}</style>
                 <p className="MuiTypography-root MuiTypography-body1 css-pheo6p">
                   <span>
                     The world’s first tourism destination on an offshore <br /> platform inspired by
@@ -60,11 +215,51 @@ export const Default = (props: TheRigIntroProps): JSX.Element => {
                   </span>
                 </p>
                 <div className="MuiGrid-root MuiGrid-direction-xs-row css-124fws5">
-                  <style data-emotion="css e35sq3">{`.css-e35sq3{background-color:rgba(255, 255, 255, 0.05);color:#FFFFFF;border-radius:2px;display:-webkit-inline-box;display:-webkit-inline-flex;display:-ms-inline-flexbox;display:inline-flex;padding:12px 32px;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;gap:8px;border:1px solid #FFFFFF;margin-top:60px;cursor:pointer;font-size:18px;font-style:normal;font-weight:500;line-height:160%;}.css-e35sq3:active{background-color:#FFFFFF;border:2px solid #FFFFFF;color:#FFFFFF;}@media screen and (max-width: 899px){.css-e35sq3{margin-top:48px;}}`}</style>
+                  <style jsx data-emotion="css e35sq3">{`
+                    .css-e35sq3 {
+                      background-color: rgba(255, 255, 255, 0.05);
+                      color: #ffffff;
+                      border-radius: 2px;
+                      display: -webkit-inline-box;
+                      display: -webkit-inline-flex;
+                      display: -ms-inline-flexbox;
+                      display: inline-flex;
+                      padding: 12px 32px;
+                      -webkit-align-items: center;
+                      -webkit-box-align: center;
+                      -ms-flex-align: center;
+                      align-items: center;
+                      gap: 8px;
+                      border: 1px solid #ffffff;
+                      margin-top: 60px;
+                      cursor: pointer;
+                      font-size: 18px;
+                      font-style: normal;
+                      font-weight: 500;
+                      line-height: 160%;
+                    }
+                    .css-e35sq3:active {
+                      background-color: #ffffff;
+                      border: 2px solid #ffffff;
+                      color: #ffffff;
+                    }
+                    @media screen and (max-width: 899px) {
+                      .css-e35sq3 {
+                        margin-top: 48px;
+                      }
+                    }
+                  `}</style>
                   <button type="submit" style={{ display: 'inline-flex' }} className="css-e35sq3">
                     <span>
                       SUBSCRIBE
-                      <style data-emotion="css iiupvq">{`.css-iiupvq{width:10px;margin:0 5px;vertical-align:middle;height:10px;}`}</style>
+                      <style jsx data-emotion="css iiupvq">{`
+                        .css-iiupvq {
+                          width: 10px;
+                          margin: 0 5px;
+                          vertical-align: middle;
+                          height: 10px;
+                        }
+                      `}</style>
                       <img
                         alt=""
                         loading="lazy"
@@ -79,7 +274,19 @@ export const Default = (props: TheRigIntroProps): JSX.Element => {
                     </span>
                   </button>
                 </div>
-                <style data-emotion="css 11ant8a">{`.css-11ant8a{width:38px;height:51px;margin-top:90px;display:none;}@media screen and (min-width: 900px){.css-11ant8a{display:inline;}}`}</style>
+                <style jsx data-emotion="css 11ant8a">{`
+                  .css-11ant8a {
+                    width: 38px;
+                    height: 51px;
+                    margin-top: 90px;
+                    display: none;
+                  }
+                  @media screen and (min-width: 900px) {
+                    .css-11ant8a {
+                      display: inline;
+                    }
+                  }
+                `}</style>
                 <img
                   alt="A world-first in adventure tourism"
                   loading="lazy"
@@ -96,10 +303,51 @@ export const Default = (props: TheRigIntroProps): JSX.Element => {
             </div>
           </div>
         </div>
-        <style data-emotion="css zqttgr">{`.css-zqttgr{position:absolute;min-width:100%;width:1985px;height:120px;bottom:0px;z-index:1;background:linear-gradient(180deg, rgba(1, 6, 13, 0.00) 0%, #01060D 100%);}`}</style>
-        <style data-emotion="css 1peekm2">{`.css-1peekm2{box-sizing:border-box;-webkit-flex-direction:row;-ms-flex-direction:row;flex-direction:row;position:absolute;min-width:100%;width:1985px;height:120px;bottom:0px;z-index:1;background:linear-gradient(180deg, rgba(1, 6, 13, 0.00) 0%, #01060D 100%);}`}</style>
+        <style jsx data-emotion="css zqttgr">{`
+          .css-zqttgr {
+            position: absolute;
+            min-width: 100%;
+            width: 1985px;
+            height: 120px;
+            bottom: 0px;
+            z-index: 1;
+            background: linear-gradient(180deg, rgba(1, 6, 13, 0) 0%, #01060d 100%);
+          }
+        `}</style>
+        <style jsx data-emotion="css 1peekm2">{`
+          .css-1peekm2 {
+            box-sizing: border-box;
+            -webkit-flex-direction: row;
+            -ms-flex-direction: row;
+            flex-direction: row;
+            position: absolute;
+            min-width: 100%;
+            width: 1985px;
+            height: 120px;
+            bottom: 0px;
+            z-index: 1;
+            background: linear-gradient(180deg, rgba(1, 6, 13, 0) 0%, #01060d 100%);
+          }
+        `}</style>
         <div className="MuiGrid-root css-1peekm2"></div>
-        <style data-emotion="css mizbsd">{`.css-mizbsd{background:url('/rust/rust-top-straight.webp'),transparent 50%/cover no-repeat;mix-blend-mode:plus-darker;width:2000px;height:166px;-webkit-filter:brightness(0);filter:brightness(0);left:-280px;position:absolute;bottom:-84px;}@media screen and (min-width: 1921px){.css-mizbsd{left:0;}}`}</style>
+        <style jsx data-emotion="css mizbsd">{`
+          .css-mizbsd {
+            background: url('/rust/rust-top-straight.webp'), transparent 50% / cover no-repeat;
+            mix-blend-mode: plus-darker;
+            width: 2000px;
+            height: 166px;
+            -webkit-filter: brightness(0);
+            filter: brightness(0);
+            left: -280px;
+            position: absolute;
+            bottom: -84px;
+          }
+          @media screen and (min-width: 1921px) {
+            .css-mizbsd {
+              left: 0;
+            }
+          }
+        `}</style>
         <div className="css-mizbsd"></div>
       </div>
     );
