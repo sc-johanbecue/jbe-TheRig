b4d44188e1671818b1af95f4e027d6eaa2e3e26a
sc-johanbecue <johan.becue@sitecore.com>
2024-04-02 15:19:50 +0200

    modified:   src/sxastarter/src/components/RollsRoyce/RollsRoyceSignPost.tsx

--- src/sxastarter/src/components/RollsRoyce/RollsRoyceSignPost.tsx
+++ src/sxastarter/src/components/RollsRoyce/RollsRoyceSignPost.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { ImageField, TextField, RichTextField } from '@sitecore-jss/sitecore-jss-nextjs';
-import Link from 'next/link';
-import Image from 'next/image';
+import {
+  ImageField,
+  TextField,
+  LinkField,
+  Link as JssLink,
+  Image as JssImage,
+  Text,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
-  Number: TextField;
-  Label: TextField;
-  Name: TextField;
-  Category: TextField;
-  Family: TextField;
-  ShortDescription: RichTextField;
-  LongDescription: RichTextField;
-  MasterAsset: ImageField;
+  Title: TextField;
+  Text: TextField;
+  Cta: LinkField;
+  Image: ImageField;
 }
-
 type RollsRoyceSignPostProps = {
   params: { [key: string]: string };
   fields: Fields;
@@ -32,34 +32,24 @@ export const Default = (props: RollsRoyceSignPostProps): JSX.Element => {
     return (
       <>
         <div className="ts-image">
-          <Image
-            src="https://www.rolls-royce.com/~/media/Images/R/Rolls-Royce/signposts/power-of-trent-signpost.jpg?h=375&amp;iar=0&amp;w=570"
-            alt=""
-            width="570"
-            height="375"
-          />
+          <JssImage width="570" height="375" field={props.fields.Image} />
         </div>
         <div className="ts-content">
           <div className="tsVcenter">
             <div className="tsHover">
               <div className="three-sp-title">
-                <h4 className="mixbold font21">Power of Trent</h4>
+                <h4 className="mixbold font21">
+                  <Text field={props.fields.Title} />
+                </h4>
               </div>
               <div className="font18 mixlight">
                 <p>
-                  For nearly three decades, our family of turbofan aircraft engines have continued
-                  to push the boundaries of what is possible, as each new model sets new performance
-                  benchmarks for civil aviation.
+                  <Text field={props.fields.Text} />
                 </p>
               </div>
               <div className="in-signpost">
                 <span className="rad-link">
-                  <Link
-                    href="https://www.rolls-royce.com/products-and-services/civil-aerospace/widebody/power-of-trent.aspx"
-                    className="attachment-link"
-                  >
-                    LEARN MORE
-                  </Link>
+                  <JssLink field={props.fields.Cta} />
                 </span>
               </div>
             </div>
