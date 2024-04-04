df2bd2de4a778ec1bc24b1e25876dce5d140b9aa
sc-johanbecue <johan.becue@sitecore.com>
2024-04-04 11:11:22 +0200

    modified:   src/sxastarter/src/components/RollsRoyce/RollsRoyceSubSection.tsx

--- src/sxastarter/src/components/RollsRoyce/RollsRoyceSubSection.tsx
+++ src/sxastarter/src/components/RollsRoyce/RollsRoyceSubSection.tsx
@@ -34,8 +34,8 @@ const RollsRoyceSubSectionDefaultComponent = (props: RollsRoyceSubSectionProps):
 );
 
 export const Default = (props: RollsRoyceSubSectionProps): JSX.Element => {
-  const backgroundStyle = `url(${props.fields.Image.value?.src} no-repeat scroll center center`;
-  //center center / cover
+  const backgroundStyle = `url(${props.fields.Image.value?.src}) center center / cover`;
+  //no-repeat scroll center center
 
   if (props.fields) {
     return (
