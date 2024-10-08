f485e7efc63417f5883d4d83213cb06ba024d33b
sc-johanbecue <johan.becue@sitecore.com>
2024-10-08 17:42:22 +0200

    modified:   src/sxastarter/src/Layout.tsx

--- src/sxastarter/src/Layout.tsx
+++ src/sxastarter/src/Layout.tsx
@@ -54,6 +54,7 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
 
       {/* root placeholder for the app, which we add components to using route data */}
       <div className={mainClassPageEditing}>
+        <div>Name: {layoutData.sitecore.context.site?.name}</div>
         <header>
           <div id="header">{route && <Placeholder name="headless-header" rendering={route} />}</div>
         </header>
