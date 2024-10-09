f84df011dd12f690b7e6d327a9ebd438c0ec6876
sc-johanbecue <johan.becue@sitecore.com>
2024-10-09 12:21:58 +0200

    modified:   src/sxastarter/src/LayoutDubaiWTC.tsx 	new file:   src/sxastarter/src/components/DubaiWTC/DubaiWTCFooter.tsx

---
+++ src/sxastarter/src/components/DubaiWTC/DubaiWTCFooter.tsx
@@ -0,0 +1,144 @@
+import Link from 'next/link';
+import React from 'react';
+
+const DubaiWTCStandDetails = (): JSX.Element => {
+  return (
+    <>
+      {/* <div className="col-xs-12 section-padding warning-section footer_warning text-center"> */}
+      {/* <p className="red-text ancerlink-color"><b>WARNING FOR EXHIBITORS - MISLEADING SALES</b></p>
+                  <p className="text-gray">Please be warned that the below companies are approaching exhibitors with an offer to sell supposed past attendee data and/or exhibitor directory services.</p>
+                  <div align="center">
+                      <ul className="points ancerlink-color">
+                          <li>Eprospectmedia.com/Global Zest Business Solutions</li>
+                          <li>International Fairs Directory</li>
+                          <li>EListAppenders/ELA</li>
+                          <li>InfoCapp</li>
+                          <li>tradeeventslist.com</li>
+                          <li>Amy Gibbs</li>
+                          <li>Events Info Group - Global Events Partner</li>
+
+                      </ul>
+                  </div>
+                  <p className="text-gray">We assure you that the above mentioned organizations do not possess any such data.<br />
+                      If you are approached or have any questions, feel free to contact us on: <span className="whitelinks">T:</span> <Link data-type='a' href="tel:97144380355" className="whitelinks">XXX X XXX XXXX</Link> | <span className="whitelinks">E:</span> <Link data-type='a' href="mailto:no-reply@xporience.com" className="whitelinks">no-reply@xporience.com</Link> </p>
+
+                  */}
+      {/* <p>
+                  <div className="col-lg-4 col-md-4 warn-bar ">
+                      <div className="col-lg-12 col-md-12 footer_warn_bar">
+                          <b className="red-text warn-btn  ancerlink-color"> WARNING!</b> <br /><br />
+                          <b className="red-text warn-text ancerlink-color"> Beware of email scams</b>
+                      </div>
+                  </div>
+                  <div className="col-lg-8 col-md-8 warn-side ">
+                      Please notify us immediately on <b><Link data-type='a' className="whitelinks" href="mailto:no-reply@xporience.com">no-reply@xporience.com</Link></b> if you receive any emails falsely claiming to sell attendee data or acting as third-party representatives for the exhibitor catalogue/show directory.
+                      <br />
+                      <br />
+                      Beware in any dealings with the following companies/domains<br />
+                  </p>
+                  </div>
+                  <div className="col-lg-12 col-md-12 footerlinks_wrp" align="center">
+                  <ul className="col-lg-12 col-md-12  warn-sites ancerlink-color">
+                  <li> <i className="square"></i>International Fairs Directory (Inter-Fairs.com)
+                  <li><i className="square"></i>Expo Guide, FairGuide.com
+                  <li><i className="square"></i>ConstructDataVerlag.com
+                  <li><i className="square"></i>MarketingInfoZone.com
+                  <li><i className="square"></i>ZoomAppend.com
+                  <li><i className="square"></i> DataMarketMedia.com
+                  <li><i className="square"></i>WisdowTargets.com
+                  <li><i className="square"></i>WWTechData.com
+                  </ul>
+                  </div> */}
+      {/* </div> */}
+      <div id="footer" className="dwtcfooter">
+        <div className="container" style={{ padding: '25px !important' }}>
+          {/* <div className="row">
+                        <div className="col-lg-12 text-center dwtclogo"><Link data-type='a' href="http://www.dwtc.com"  target="_blank" rel="noopener noreferrer" ><img width="231" src="https://dwtc.exhibitoronlinemanual.com/themes/gitex_theme/images/logo-w.png" alt="" /></Link></div>
+                        </div> */}
+          {/* <div className="row">
+                        <hr className="style1">
+                        </div> */}
+          {/* <div id="overlay" className="row" style="padding-left: 0; padding-right: 0">
+                        <div id="overlay">
+                            <div className="col-md-1 text-center resetl social_icons">
+                                <div className="leftbox">
+                                    <table width="120" border="0" cellspacing="0" cellpadding="5" className="social" align="center">
+                                        <tbody>
+                                            <tr>
+                                                <td><Link data-type='a' href="#"  target="_blank" rel="noopener noreferrer" ><img src="https://dwtc.exhibitoronlinemanual.com/themes/gitex_theme/images/linkedin.svg" width="30" alt="" /></Link></td>
+                                                <td><Link data-type='a' href="#"  target="_blank" rel="noopener noreferrer" ><img src="https://dwtc.exhibitoronlinemanual.com/themes/gitex_theme/images/facebook.svg" width="30" alt="" /></Link></td>
+                                                <td><Link data-type='a' href="#"  target="_blank" rel="noopener noreferrer" ><img src="https://dwtc.exhibitoronlinemanual.com/themes/gitex_theme/images/twitter.svg" width="30" alt="" /></Link></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div className="col-md-9 col-sm-12 text-center blurp middle_text"><Link data-type='a' href="#"  target="_blank" rel="noopener noreferrer" >Click here</Link> to view our data protection and privacy policy. | Online Exhibitor Manual Developed by: : <Link data-type='a' href="http://www.xporience.com"  target="_blank" rel="noopener noreferrer" >www.xporience.com</Link></div>
+                            <div className="col-md-2 resetr middle_text_inner">
+                                <div className="resetr"><img src="https://dwtc.exhibitoronlinemanual.com/themes/gitex_theme/images/aeo-ufi.svg" alt="" width="168" height="44" usemap="#Map" />
+                                    <map name="Map">
+                                        <area shape="rect" coords="82,4,132,43" href="https://www.aeo.org.uk/"  target="_blank" rel="noopener noreferrer" >
+                                        <area shape="rect" coords="143,3,171,49" href="http://www.ufi.org/"  target="_blank" rel="noopener noreferrer" >
+                                    </map>
+                                </div>
+                            </div>
+                        </div>
+                        </div> */}
+          {/* <div className="row">
+                        <hr className="style1">
+                        </div> */}
+          <div className="row overlaymob">
+            <div className="col-md-12 blurp text-center">
+              <Link
+                data-type="a"
+                href="http://www.dwtc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  width="50px"
+                  height="auto"
+                  src="https://dwtc.exhibitoronlinemanual.com/themes/gitex_theme/images/logo-w.png"
+                  alt=""
+                />
+              </Link>
+            </div>
+            <div className="col-md-12 text-center blurp" style={{ marginTop: '10px;' }}>
+              <span>
+                Click here for{' '}
+                <Link
+                  data-type="a"
+                  href="https://www.dwtc.com/en/terms/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Terms and Conditions
+                </Link>{' '}
+                and{' '}
+                <Link
+                  data-type="a"
+                  href="https://www.dwtc.com/en/privacy/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Policies
+                </Link>{' '}
+                &#124; Online Exhibitor Manual developed by Xporience &#124;{' '}
+                <Link
+                  data-type="a"
+                  href="https://xporience.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  www.xporience.com
+                </Link>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DubaiWTCStandDetails;
