beabc4c11fd7bf5b006d405e144f8d7c8e35d9f9
sc-johanbecue <johan.becue@sitecore.com>
2024-10-18 11:36:02 +0200

    modified:   src/sxastarter/src/LayoutJumeirah.tsx

--- src/sxastarter/src/LayoutJumeirah.tsx
+++ src/sxastarter/src/LayoutJumeirah.tsx
@@ -13,7 +13,6 @@ import {
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import config from 'temp/config';
 import Scripts from 'src/Scripts';
-import Link from 'next/link';
 
 // Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
 // If you're not supporting the Experience Editor, you can remove this.
