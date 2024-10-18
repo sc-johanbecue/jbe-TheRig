2ba9b373d69acbaaa7c97003e3e649ff45d5830a
sc-johanbecue <johan.becue@sitecore.com>
2024-10-18 11:44:24 +0200

    modified:   src/sxastarter/src/LayoutJumeirah.tsx

--- src/sxastarter/src/LayoutJumeirah.tsx
+++ src/sxastarter/src/LayoutJumeirah.tsx
@@ -181,7 +181,225 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
         <div id="root">
           <div className=" container-fluid   false home      ">
             <div className="  ">
-              <div className="HeaderSection w-100 p-0 index-4 "></div>
+              <div className="HeaderSection w-100 p-0 index-4 ">
+                <div>
+                  <div className="main-header " style={{ display: 'block' }}>
+                    <div className="primary-header">
+                      <div className="menu-icon d-block d-md-none">
+                        <img
+                          src="/static/media/white-hamburger.fe154352164530b63dd61630741a1f0a.svg"
+                          alt="menu"
+                          style={{ height: '24px', width: '24px' }}
+                        />
+                      </div>
+                      <div className="search-icon left-wrapper">
+                        <img
+                          src="/static/media/white-search.162267a5520f0668860de15b2e658b9a.svg"
+                          alt="menu"
+                          style={{ height: '24px', width: '24px' }}
+                        />
+                      </div>
+                      <div className="jum-logo">
+                        <a href="/en">
+                          <img
+                            src="https://cdn.jumeirah.com/-/mediadh/dh/hospitality/jumeirah/logo/redesign/jumeirah_logo_whitesvg.svg?modified=20240428154803"
+                            alt=""
+                          />
+                        </a>
+                      </div>
+                      <div className="action-items d-flex items-center">
+                        <div className="items nav-bar ">
+                          <div className="jumeirah-language-selector dropdown">
+                            <button
+                              type="button"
+                              aria-haspopup="true"
+                              aria-expanded="false"
+                              className="btn btn-secondary"
+                            >
+                              English
+                            </button>
+                            <div
+                              tabIndex={-1}
+                              role="menu"
+                              aria-hidden="true"
+                              className="dropdown-menu"
+                            >
+                              <button
+                                type="button"
+                                tabIndex={0}
+                                role="menuitem"
+                                className="dropdown-item"
+                              >
+                                <a>العربـية</a>
+                              </button>
+                              <button
+                                type="button"
+                                tabIndex={0}
+                                role="menuitem"
+                                className="dropdown-item"
+                              >
+                                <a></a>
+                              </button>
+                              <button
+                                type="button"
+                                tabIndex={0}
+                                role="menuitem"
+                                className="dropdown-item"
+                              >
+                                <a>Deutsch</a>
+                              </button>
+                              <button
+                                type="button"
+                                tabIndex={0}
+                                role="menuitem"
+                                className="dropdown-item"
+                              >
+                                <a>Français</a>
+                              </button>
+                              <button
+                                type="button"
+                                tabIndex={0}
+                                role="menuitem"
+                                className="dropdown-item"
+                              >
+                                <a>Русский</a>
+                              </button>
+                              <button
+                                type="button"
+                                tabIndex={0}
+                                role="menuitem"
+                                className="dropdown-item"
+                              >
+                                <a>简体中文</a>
+                              </button>
+                              <button
+                                type="button"
+                                tabIndex={0}
+                                role="menuitem"
+                                className="dropdown-item"
+                              >
+                                <a>Español</a>
+                              </button>
+                              <button
+                                type="button"
+                                tabIndex={0}
+                                role="menuitem"
+                                className="dropdown-item"
+                              >
+                                <a>Italiano</a>
+                              </button>
+                              <button
+                                type="button"
+                                tabIndex={0}
+                                role="menuitem"
+                                className="dropdown-item"
+                              >
+                                <a>Indonesian</a>
+                              </button>
+                            </div>
+                          </div>
+                          <div className="lang-selector-arrow"></div>
+                        </div>
+                        <div className="items ">
+                          <div className="login-button flex-row">
+                            <div className="profile-icon">
+                              <img
+                                src="/static/media/white-profile.05db31ac4360eefca1e6d94842b6c8e4.svg"
+                                alt=""
+                              />
+                            </div>
+                            <div>
+                              <div
+                                id="jone-login"
+                                className="login-wrapper col-sm-12 col-lg-4 col-md-12  "
+                              >
+                                <div className="login-link text-white">
+                                  <span className="">Login</span>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="items ">My reservation</div>
+                        <div className="items">
+                          <button type="button" className="book-now btn btn-secondary">
+                            RESERVE
+                          </button>
+                        </div>
+                      </div>
+                      &nbsp;
+                    </div>
+                    <div className="secondary-header">
+                      <ul className="SecondaryNavRD navbar-nav">
+                        <li className="nav-item">
+                          <a href="/en/stay" target="" title="" rel="" className="nav-link">
+                            <span className="list-item ">Hotels &amp; Resorts</span>
+                          </a>
+                        </li>
+                        <li className="nav-item">
+                          <a
+                            href="/en/suites-and-villas"
+                            target=""
+                            title=""
+                            rel=""
+                            className="nav-link"
+                          >
+                            <span className="list-item ">Suites &amp; Villas</span>
+                          </a>
+                        </li>
+                        <li className="nav-item">
+                          <a href="/en/dine/listing" target="" title="" rel="" className="nav-link">
+                            <span className="list-item ">Dining</span>
+                          </a>
+                        </li>
+                        <li className="nav-item">
+                          <a
+                            href="/en/rejuvenate/listing"
+                            target=""
+                            title=""
+                            rel=""
+                            className="nav-link"
+                          >
+                            <span className="list-item ">Well-being</span>
+                          </a>
+                        </li>
+                        <li className="nav-item">
+                          <a
+                            href="/en/article/inspiration"
+                            target=""
+                            title=""
+                            rel=""
+                            className="nav-link"
+                          >
+                            <span className="list-item ">Inspiration</span>
+                          </a>
+                        </li>
+                        <li className="nav-item">
+                          <a href="/en/offers" target="" title="" rel="" className="nav-link">
+                            <span className="list-item ">Exclusive Offers</span>
+                          </a>
+                        </li>
+                        <li className="nav-item">
+                          <a
+                            href="/en/events/listing"
+                            target=""
+                            title=""
+                            rel=""
+                            className="nav-link"
+                          >
+                            <span className="list-item ">Events</span>
+                          </a>
+                        </li>
+                        <li className="nav-item">
+                          <a href="/en/guest-user" target="" title="" rel="" className="nav-link">
+                            <span className="list-item ">Jumeirah One</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="  ">
               <div
