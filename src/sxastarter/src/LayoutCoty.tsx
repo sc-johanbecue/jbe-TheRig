966dc0e73be8075c51f0ce59127ecb897dc8d948
sc-johanbecue <johan.becue@sitecore.com>
2025-01-02 11:38:26 +0100

    modified:   src/sxastarter/src/LayoutCoty.tsx

--- src/sxastarter/src/LayoutCoty.tsx
+++ src/sxastarter/src/LayoutCoty.tsx
@@ -1,10 +1,56 @@
+/**
+ * This Layout is needed for Starter Kit.
+ */
+import React from 'react';
 import Head from 'next/head';
+import {
+  Placeholder,
+  LayoutServiceData,
+  Field,
+  HTMLLink,
+  ImageField,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+import config from 'temp/config';
+import Scripts from 'src/Scripts';
 
-export default function Home() {
+// Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
+// If you're not supporting the Experience Editor, you can remove this.
+const publicUrl = config.publicUrl;
+
+interface LayoutProps {
+  layoutData: LayoutServiceData;
+  headLinks: HTMLLink[];
+}
+
+interface RouteFields {
+  [key: string]: unknown;
+  Title?: Field;
+  OgTitle?: Field;
+  OgDescription?: Field;
+  OgImage?: ImageField;
+}
+
+const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
+  const { route } = layoutData.sitecore;
+  const fields = route?.fields as RouteFields;
+  const isPageEditing = layoutData.sitecore.context.pageEditing;
+  const mainClassPageEditing = isPageEditing ? 'editing-mode' : 'prod-mode';
+
   return (
     <>
+      <Scripts />
       <Head>
-        <title>Burberry - Prestige Brands - Coty</title>
+        <title>{fields?.Title?.value?.toString() || 'Page'}</title>
+        {/* <title>Burberry - Prestige Brands - Coty</title> */}
+        <link rel="icon" href={`${publicUrl}/favicon.ico`} />
+        {headLinks.map((headLink) => (
+          <link rel={headLink.rel} key={headLink.href} href={headLink.href} />
+        ))}
+        <link rel="canonical" href={publicUrl} />
+        <meta property="og:title" content={fields?.OgTitle?.value?.toString()} />
+        <meta property="og:description" content={fields?.OgDescription?.value?.toString()} />
+        <meta property="og:image" content={fields?.OgImage?.value?.src?.toString()} />
+        <meta property="og:type" content={route?.templateName} data-attr="JSS-21.6.0" />
         <meta
           name="description"
           content="British luxury fashion house established in 1856 by Thomas Burberry headquartered in London, England."
@@ -25,7 +71,19 @@ export default function Home() {
         <script async src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"></script>
       </Head>
 
-      <div id="__next">
+      {/* root placeholder for the app, which we add components to using route data */}
+      <div className={mainClassPageEditing}>
+        <div>Name: {layoutData.sitecore.context.site?.name}</div>
+        <header>
+          <div id="header">{route && <Placeholder name="headless-header" rendering={route} />}</div>
+        </header>
+        <main>
+          <div id="content">{route && <Placeholder name="headless-main" rendering={route} />}</div>
+        </main>
+        <footer>
+          <div id="footer">{route && <Placeholder name="headless-footer" rendering={route} />}</div>
+        </footer>
+
         <a href="#content" className="absolute left-[-9999px] focus:left-0 focus:top-[98px]">
           Skip to content
         </a>
@@ -711,4 +769,6 @@ export default function Home() {
       </div>
     </>
   );
-}
+};
+
+export default Layout;
