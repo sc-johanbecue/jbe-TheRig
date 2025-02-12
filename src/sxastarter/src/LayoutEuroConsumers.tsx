3003996c5f537d496cbd9bb1b7c7e9a968b80a1e
sc-johanbecue <johan.becue@sitecore.com>
2025-02-12 23:48:24 +0100

    modified:   src/sxastarter/src/LayoutEuroConsumers.tsx

--- src/sxastarter/src/LayoutEuroConsumers.tsx
+++ src/sxastarter/src/LayoutEuroConsumers.tsx
@@ -467,6 +467,17 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
           src="//tglyr.co/latest.js?n=o"
           className="optanon-category-C0003"
         ></script>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
+        />
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+          crossOrigin="anonymous"
+        />
       </Head>
 
       {/* root placeholder for the app, which we add components to using route data */}
